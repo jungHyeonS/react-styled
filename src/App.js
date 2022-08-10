4b9e11950f3d7b16c8f1664c3fdc524f76013396
jungHyeonS <fbtls25@naver.com>
2022-08-10 22:52:21 +0900

    react-master-class : styled 컴포넌트 확장

--- src/App.js
+++ src/App.js
@@ -7,14 +7,8 @@ const Father = styled.div`
   display:flex;
 `
 
-const BoxOne = styled.div`
-  background-color:teal;
-  width:100px;
-  height:100px;
-`
-
-const BoxTwo = styled.div`
-  background-color:tomato;
+const Box = styled.div`
+  background-color:${props => props.bgColor};
   width:100px;
   height:100px;
 `
@@ -24,13 +18,18 @@ const Text = styled.span`
 `
 
 
+//Box 에 있는 모든 스타일을 들고오고 지정된 속성을 추가한다(컴포넌트 확장)
+const Circle = styled(Box)`
+  border-radius: 50px;
+`
+
 function App() {
   return (
     <Father>
-      <BoxOne>
+      <Box bgColor="teal">
         <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo/>
+      </Box>
+      <Circle bgColor="tomato"/>
     </Father>
   );
 }
