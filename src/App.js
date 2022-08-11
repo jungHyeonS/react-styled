863308def36fc6f896cd08215e564d44544fa327
jungHyeonS <fbtls25@naver.com>
2022-08-11 22:12:05 +0900

    react-master-class : animation & pseudo selectors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
-import styled from "styled-components"
+import styled,{keyframes} from "styled-components"
 
 
-//back tick 무조건 사용
+/*back tick 무조건 사용
 const Father = styled.div`
   display:flex;
 `
@@ -18,7 +18,7 @@ const Text = styled.span`
 `
 
 
-//Box 에 있는 모든 스타일을 들고오고 지정된 속성을 추가한다(컴포넌트 확장)
+Box 에 있는 모든 스타일을 들고오고 지정된 속성을 추가한다(컴포넌트 확장)
 const Circle = styled(Box)`
   border-radius: 50px;
 `
@@ -30,32 +30,89 @@ const Btn = styled.button`
   border-radius: 15px;
 `
 
-//해당 컴포넌트에 속성을 지정
+해당 컴포넌트에 속성을 지정
 const Input = styled.input.attrs({require:true})`
   background-color: tomato;
-`
+`*/
+
+
+
+/*<Father>
+      <Box bgColor="teal">
+        <Text>Hello</Text>
+      </Box>
+      <Circle bgColor="tomato"/>
+
+      <Btn>Login</Btn>
+
+      as - 컴포넌트를 지정한 html 로 바꾼다
+      <Btn as="a" href="/">Login</Btn>
+
+      <Input/>
+      <Input/>
+      <Input/>
+      <Input/>
+      <Input/>
+      <Input/>
+     </Father>*/
+
+
+const Wrapper = styled.div`
+display: flex;
+`;
+
+const animation = keyframes`
+  0%{
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50%{
+    transform: rotate(360deg);
+    border-radius: 100px;
+  }
+  100%{
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+`;
+
+const Emoji = styled.span`
+font-size: 36px;
+`;
+
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  animation: ${animation} 1s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* span{
+    &:hover{
+      font-size: 98px;
+    }
+    &:active{
+      opacity: 0;
+    }
+  } */
+  ${Emoji}{
+    &:hover{
+      font-size: 98px;
+    }
+  }
+`;   
+
 
 
 function App() {
   return (
-    <Father>
-      {/* <Box bgColor="teal"> */}
-        {/* <Text>Hello</Text> */}
-      {/* </Box> */}
-      {/* <Circle bgColor="tomato"/> */}
-
-      {/* <Btn>Login</Btn>
-
-      as - 컴포넌트를 지정한 html 로 바꾼다
-      <Btn as="a" href="/">Login</Btn> */}
-
-      <Input/>
-      <Input/>
-      <Input/>
-      <Input/>
-      <Input/>
-      <Input/>
-    </Father>
+     <Wrapper>
+        <Box>
+          <Emoji as="p">happy</Emoji>
+        </Box>
+     </Wrapper>
   );
 }
 
